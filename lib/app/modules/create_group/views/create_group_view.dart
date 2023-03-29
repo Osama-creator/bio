dc3ffb1e16fa99280524983890da7ff54bac816b
Osama-creatore <osskh1221@gmail.com>
2023-03-29 22:54:45 +0200

    new U2

--- lib/app/modules/create_group/views/create_group_view.dart
+++ lib/app/modules/create_group/views/create_group_view.dart
@@ -73,6 +73,7 @@ class CreateGroupView extends GetView<CreateGroupController> {
                     thickness: 1,
                   ),
                 ),
+                AddStudent(controller: controller),
                 Expanded(
                   child: ListView.builder(
                     itemCount: _.students.length,
@@ -85,65 +86,6 @@ class CreateGroupView extends GetView<CreateGroupController> {
                         ),
                       );
                     },
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: SizedBox(
-                    height: context.height * 0.07,
-                    width: context.width * 0.8,
-                    child: ElevatedButton(
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          barrierDismissible: false,
-                          builder: (BuildContext context) {
-                            return AlertDialog(
-                              content: Padding(
-                                padding: const EdgeInsets.all(8.0),
-                                child: Column(
-                                  mainAxisSize: MainAxisSize.min,
-                                  children: [
-                                    MyTextFeild(
-                                      width: context.width * 0.8,
-                                      controller: _.studentNameController,
-                                      hintText: 'أدخل إسم الطالب',
-                                      labelText: "إسم الطالب",
-                                      onFieldSubmitted: (_) {
-                                        false;
-                                      },
-                                    ),
-                                    MyTextFeild(
-                                      width: context.width * 0.8,
-                                      controller: _.studentPriceController,
-                                      hintText: 'أدخل سعر الطالب (اختياري)',
-                                      labelText: 'سعر الطالب (اختياري)',
-                                    ),
-                                  ],
-                                ),
-                              ),
-                              actionsAlignment: MainAxisAlignment.start,
-                              actions: [
-                                ElevatedButton(
-                                  child: Text("إضافه",
-                                      style: context.textTheme.headline1),
-                                  onPressed: () {
-                                    controller.addStudent();
-                                    Navigator.of(context).pop();
-                                  },
-                                ),
-                              ],
-                            );
-                          },
-                        );
-                      },
-                      child: Text(
-                        'إضافه طالب',
-                        style: context.textTheme.headline6!.copyWith(
-                          fontSize: 18,
-                        ),
-                      ),
-                    ),
                   ),
                 ),
                 Padding(
@@ -165,6 +107,76 @@ class CreateGroupView extends GetView<CreateGroupController> {
               ],
             );
           }),
+    );
+  }
+}
+
+class AddStudent extends StatelessWidget {
+  const AddStudent({
+    Key? key,
+    required this.controller,
+  }) : super(key: key);
+
+  final CreateGroupController controller;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.symmetric(horizontal: context.width * 0.2),
+      child: SizedBox(
+        height: context.height * 0.05,
+        child: ElevatedButton(
+          onPressed: () {
+            showDialog(
+              context: context,
+              barrierDismissible: false,
+              builder: (BuildContext context) {
+                return AlertDialog(
+                  content: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        MyTextFeild(
+                          width: context.width * 0.8,
+                          controller: controller.studentNameController,
+                          hintText: 'أدخل إسم الطالب',
+                          labelText: "إسم الطالب",
+                          onFieldSubmitted: (_) {
+                            false;
+                          },
+                        ),
+                        MyTextFeild(
+                          width: context.width * 0.8,
+                          controller: controller.studentPriceController,
+                          hintText: 'أدخل سعر الطالب (اختياري)',
+                          labelText: 'سعر الطالب (اختياري)',
+                        ),
+                      ],
+                    ),
+                  ),
+                  actionsAlignment: MainAxisAlignment.start,
+                  actions: [
+                    ElevatedButton(
+                      child: Text("إضافه", style: context.textTheme.headline1),
+                      onPressed: () {
+                        controller.addStudent();
+                        Navigator.of(context).pop();
+                      },
+                    ),
+                  ],
+                );
+              },
+            );
+          },
+          child: Text(
+            'إضافه طالب',
+            style: context.textTheme.headline6!.copyWith(
+              fontSize: 18,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
