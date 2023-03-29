b9be6334f9b7a36c1594d706b1fa3110d753bce7
Osama-creatore <osskh1221@gmail.com>
2023-03-29 22:53:03 +0200

    new U

--- lib/app/modules/create_exam/controllers/create_exam_controller.dart
+++ lib/app/modules/create_exam/controllers/create_exam_controller.dart
@@ -1,23 +1,9 @@
+import 'package:bio/app/data/models/question_model.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class CreateExamController extends GetxController {
-  //TODO: Implement CreateExamController
-
-  final count = 0.obs;
-  @override
-  void onInit() {
-    super.onInit();
-  }
-
-  @override
-  void onReady() {
-    super.onReady();
-  }
-
-  @override
-  void onClose() {
-    super.onClose();
-  }
-
-  void increment() => count.value++;
+  TextEditingController examNameController = TextEditingController();
+  RxList<Question> questions = RxList<Question>([]);
+  Future<void> createExam() async {}
 }
--- lib/app/modules/create_exam/views/create_exam_view.dart
+++ lib/app/modules/create_exam/views/create_exam_view.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
 
+import '../../../../config/utils/colors.dart';
+import '../../../views/text_field.dart';
 import '../controllers/create_exam_controller.dart';
 
 class CreateExamView extends GetView<CreateExamController> {
@@ -10,15 +12,94 @@ class CreateExamView extends GetView<CreateExamController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('CreateExamView'),
+        title: const Text('إنشاء إمتحان جديد'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'CreateExamView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: GetBuilder<CreateExamController>(
+          init: CreateExamController(),
+          builder: (_) {
+            return Column(
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: [
+                SizedBox(
+                  height: context.height * 0.01,
+                ),
+                MyTextFeild(
+                  width: context.width * 0.8,
+                  controller: _.examNameController,
+                  hintText: 'أدخل إسم الإمتحان',
+                  labelText: "إسم الإمتحان",
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: context.width * 0.1),
+                  child: const Divider(),
+                ),
+                Center(
+                  child: Text(
+                    "الأسئله",
+                    style: context.textTheme.headline6!.copyWith(
+                        color: AppColors.primary,
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: context.width * 0.1),
+                  child: const Divider(
+                    thickness: 1,
+                  ),
+                ),
+                Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: context.width * 0.2),
+                  child: SizedBox(
+                    height: context.height * 0.05,
+                    child: ElevatedButton(
+                      onPressed: _.createExam,
+                      child: Text(
+                        ' إضافه سؤال',
+                        style: context.textTheme.headline6!.copyWith(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: _.questions.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ListTile(
+                        title: Text(_.questions[index].question!),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.delete),
+                          onPressed: () {},
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SizedBox(
+                    height: context.height * 0.07,
+                    width: context.width * 0.8,
+                    child: ElevatedButton(
+                      onPressed: _.createExam,
+                      child: Text(
+                        'إنشاء المجموعة',
+                        style: context.textTheme.headline6!.copyWith(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            );
+          }),
     );
   }
 }
